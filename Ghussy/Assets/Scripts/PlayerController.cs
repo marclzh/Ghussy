1ca49c700b5bc83232ef393ecc72540ab64afe78
Eiffel-Leo-Kai-Feng <e0726757@u.nus.edu>
2022-05-17 21:01:55 +0800

    Added Player Movement

---
+++ Ghussy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // public access modifiers allows us to edit these variables directly in Unity
+    
+    public float moveSpeed = 1f; // Movement speed of character
+    public float collisionOffset = 0.025f; // Distance of collision detection
+    public ContactFilter2D movementFilter; // determines where a collision can occur (layers)
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>(); // List of collisions found during raycast 
+    bool canMove = true; // Allows us to ensure that the player cannot move while attacking
+
+    // Player Component References
+    Vector2 movementInput;
+    Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            // If movement input is not 0, try to move
+            if (movementInput != Vector2.zero)
+            {
+
+                bool success = TryMove(movementInput);
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(movementInput.x, 0));
+                }
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
+        }
+    }
+
+    
+    private bool TryMove(Vector2 direction)
+    {
+        if (direction != Vector2.zero) // null check
+        {
+            // Check for potential collisions
+            int count = rigidBody.Cast(
+                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
+
+            // If no collision, move in direction and return true, else return false
+            if (count == 0) 
+            {
+                rigidBody.MovePosition(rigidBody.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            // Can't move if there's no direction to move in
+            return false;
+        }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+    }
+
+    public void LockMovement()
+    {
+        canMove = false;
+    }
+
+    public void UnlockMovement()
+    {
+        canMove = true;
+    }
+}
