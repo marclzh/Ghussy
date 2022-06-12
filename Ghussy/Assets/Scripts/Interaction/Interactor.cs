484aeff326241bdd38144ad049087abfc3d0d386
Eiffel-Leo-Kai-Feng <e0726757@u.nus.edu>
2022-06-12 23:40:05 +0800

    Added Interaction Interface
    TODO: Interaction Text GUI logic

---
+++ Ghussy/Assets/Scripts/Interaction/Interactor.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Interactor : MonoBehaviour
+{
+    [SerializeField] private Transform interactionPoint;
+    [SerializeField] private float interactionPointRadius = 0.18f;
+    [SerializeField] private LayerMask interactableMask;
+    [SerializeField] private InteractionPromptUI interactionPromptUI;
+
+    private readonly Collider2D[] colliders = new Collider2D[3];
+    [SerializeField] private int numFound;
+    private IInteractable interactable;
+
+    private void Update()
+    {
+        ContactFilter2D cf = new ContactFilter2D();
+        cf.SetLayerMask(interactableMask);
+        numFound = Physics2D.OverlapCircle(interactionPoint.position, interactionPointRadius, cf, colliders);
+
+        if (numFound > 0)
+        {
+            interactable = colliders[0].GetComponent<IInteractable>();
+
+            if (interactable != null)
+            {
+                if (!interactionPromptUI.isDisplayed) interactionPromptUI.SetUp(interactable.InteractionPrompt);
+               
+                if (Keyboard.current.eKey.wasPressedThisFrame) interactable.Interact(this); 
+             
+            }
+        } 
+        else
+        {
+            if (interactable != null) interactable = null;
+            if (interactionPromptUI.isDisplayed) interactionPromptUI.Close();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(interactionPoint.position, interactionPointRadius);
+    }
+
+
+}
--- Ghussy/Assets/Scripts/Possession/BasePossessionState.cs
+++ Ghussy/Assets/Scripts/Possession/BasePossessionState.cs
@@ -7,5 +7,6 @@ public abstract class BasePossessionState : ScriptableObject
     [SerializeField] PlayerWeapon weapon;
     [SerializeField] PlayerAbility ability;
     public string possessionStateName;
-    []
+    [TextArea(15, 20)]
+    public string description;
 }
