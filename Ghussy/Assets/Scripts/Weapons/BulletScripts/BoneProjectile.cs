002904575fbc43f9799f4714550c64ca76842646
marclzh <uiolk@hotmail.com>
2022-06-19 18:57:52 +0800

    Weapon additions

---
+++ Ghussy/Assets/Scripts/Weapons/BulletScripts/BoneProjectile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneProjectile : BulletController
+{
+    void Start()
+    {
+        this.setDamage(20);
+    }
+}
