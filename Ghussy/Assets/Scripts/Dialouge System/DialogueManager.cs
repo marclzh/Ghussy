fa1b21896699d197d4ee55fd364e34943458fa96
Eiffel-Leo-Kai-Feng <e0726757@u.nus.edu>
2022-06-20 00:54:46 +0800

    Added Dialogue System and Opening Scene

---
+++ Ghussy/Assets/Scripts/Dialouge System/DialogueManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+	[SerializeField] VoidEvent onDialogueEnd;
+	public TextMeshProUGUI nameText;
+	public TextMeshProUGUI dialogueText;
+
+	public Animator animator;
+
+	private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start()
+	{
+		sentences = new Queue<string>();
+	}
+
+	public void StartDialogue(Dialogue dialogue)
+	{
+		animator.SetBool("IsOpen", true);
+
+		nameText.text = dialogue.name;
+
+		sentences.Clear();
+
+		foreach (string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence()
+	{
+		if (sentences.Count == 0)
+		{
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence(string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return new WaitForSeconds(.05f);
+		}
+	}
+
+	void EndDialogue()
+	{
+		animator.SetBool("IsOpen", false);
+		onDialogueEnd.Raise();
+		
+	}
+
+}
