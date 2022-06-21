aeee5f2e890ab5cc950324e600162835cb999b59
marclzh <uiolk@hotmail.com>
2022-06-21 17:30:08 +0800

    Test

--- Ghussy/Assets/Scripts/General/DestroyOnExit.cs
+++ Ghussy/Assets/Scripts/General/DestroyOnExit.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class DestroyOnExit : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Destroy(animator.gameObject, stateInfo.length);  
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    /*
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Destroy(animator.gameObject, stateInfo.length);   
     }
+    */
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
