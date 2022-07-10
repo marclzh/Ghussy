5fda69fa849b40925335d8167efffce3fd00bfd9
marclzh <uiolk@hotmail.com>
2022-07-11 02:16:58 +0800

    Refined Ability System
    - Added ability timer
    - Refined the way the ability system works

--- Ghussy/Assets/Scripts/Secondary Ability/Ability.cs
+++ Ghussy/Assets/Scripts/Secondary Ability/Ability.cs
@@ -8,9 +8,12 @@ public class Ability : ScriptableObject
     [SerializeField] private string abilityName;
     [SerializeField] private Sprite abilityImage;
     [SerializeField] private float cooldownTime;
+    [SerializeField] private float abilityTime;
 
     public virtual void Activate() { }
 
     public Sprite GetImage() { return abilityImage; }
     public float GetCooldownTime() { return cooldownTime; }
+
+    public float GetAbilityTime() { return abilityTime; }
 }
